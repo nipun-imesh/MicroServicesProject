9ad1e6e88041b26bdcd193c4b5cc08c68e99fb46
nipun imesh <imeshnipun@gmail.com>
2026-03-13 18:03:18 +0530

    add auto matic services

---
+++ automation-service/src/main/java/com/agms/automation/AutomationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.agms.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class AutomationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationServiceApplication.class, args);
+    }
+}
